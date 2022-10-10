3b0d53bb479eb79d6e67f1fb47d251fadd9ae940
AmirHossein Ebadi <awirhosein@yahoo.com>
2022-10-10 22:30:47 +0330

    Cleanup controllers

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -2,22 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\Checklist;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreTaskRequest;
 use App\Models\Task;
+use App\Models\Checklist;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\View;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\StoreTaskRequest;
 
 class TaskController extends Controller
 {
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  StoreTaskRequest $request
-     * @param Checklist $checklist
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreTaskRequest $request, Checklist $checklist)
+    public function store(StoreTaskRequest $request, Checklist $checklist): RedirectResponse
     {
         $position = $checklist->tasks->max('position') + 1;
         $checklist->tasks()->create($request->validated() + ['position' => $position]);
@@ -25,44 +20,22 @@ class TaskController extends Controller
         return redirect()->route('admin.checklist-groups.checklists.edit', [$checklist->checklist_group_id, $checklist]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  Checklist $checklist
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Checklist $checklist, Task $task)
+    public function edit(Checklist $checklist, Task $task): View
     {
         return view('admin.tasks.edit', compact('checklist', 'task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  StoreTaskRequest $request
-     * @param  Checklist $checklist
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StoreTaskRequest $request, Checklist $checklist, Task $task)
+    public function update(StoreTaskRequest $request, Checklist $checklist, Task $task): RedirectResponse
     {
         $task->update($request->validated());
 
         return redirect()->route('admin.checklist-groups.checklists.edit', [$checklist->checklist_group_id, $checklist]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Checklist $checklist
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Checklist $checklist, Task $task)
+    public function destroy(Checklist $checklist, Task $task): RedirectResponse
     {
         $checklist->tasks()->where('position', '>', $task->position)->update(
-            ['position' => \DB::raw('position - 1')]
+            ['position' => DB::raw('position - 1')]
         );
 
         $task->delete();
